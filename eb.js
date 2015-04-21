8e6659cd167ef36d86486a13afd3844415cf8be0
Ben Tillman <ben.tillman@gmail.com>
2015-04-22 11:13:11 +1200

    adds heroku support

--- eb.js
+++ eb.js
@@ -3,7 +3,7 @@ require('./server/require-jsx')();
 var config = require('./package.json').config;
 var server = require('./server');
 
-server.listen(config.port);
+server.listen(process.env.PORT || config.port);
 console.log('Listening on port %s', config.port);
 
 module.exports = server;
